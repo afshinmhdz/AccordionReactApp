c854f83d71925aa13790f21683f01f8ca531545c
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-25 22:41:05 +0330

    added accordion component and index.css style file to project

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,8 +1,32 @@
+const data = [
+  {
+    id: 1,
+    title: "Accordion One",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis cumque vel consectetur dolor, temporibus iste velit mollitia molestiae non ducimus asperiores minus! Reiciendis tenetur aspernatur ullam, explicabo deleniti optio? Sapiente!",
+  },
+  {
+    id: 2,
+    title: "Accordion two",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis cumque vel consectetur dolor, temporibus iste velit mollitia molestiae non ducimus asperiores minus! Reiciendis tenetur aspernatur ullam, explicabo deleniti optio? Sapiente!",
+  },
+  {
+    id: 3,
+    title: "Accordion three",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis cumque vel consectetur dolor, temporibus iste velit mollitia molestiae non ducimus asperiores minus! Reiciendis tenetur aspernatur ullam, explicabo deleniti optio? Sapiente!",
+  },
+];
 
-function Accordion(){
-    return (
-        <div>Accordion</div>
-    )
+function Accordion() {
+  return (
+    <div className="accordion">
+      {data.map((item) => (
+        <div className="accordion-item">
+          <div className="accordion-item__header">{item.title}</div>
+          <div className="accordion-item__content">{item.text}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Accordion
+export default Accordion;
