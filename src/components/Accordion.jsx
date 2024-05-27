4cdacc4785eeb502b7a256356b23f1cb4fb0e025
afshinmhdz <afshinmhdz1906@gmail.com>
2024-05-27 23:12:59 +0330

    added  cheveronDownicon to accordion and transtion with css

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 const data = [
   {
     id: 1,
@@ -20,13 +22,29 @@ function Accordion() {
   return (
     <div className="accordion">
       {data.map((item) => (
-        <div className="accordion-item">
-          <div className="accordion-item__header">{item.title}</div>
-          <div className="accordion-item__content">{item.text}</div>
-        </div>
+        <AccordionItem key={item.id} item={item} />
       ))}
     </div>
   );
 }
 
 export default Accordion;
+
+function AccordionItem({ item }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={`accordion-item ${isOpen ? "accordion__expanded" : ""}`}>
+      <div
+        className="accordion-item__header"
+        onClick={() => setIsOpen((is) => !is)}
+      >
+        <div>{item.title}</div>
+        <ChevronDownIcon
+          style={{ width: "1.2rem", transition: "all 0.2s ease-out", rotate:isOpen?"180deg":"0deg" }}
+        />
+      </div>
+      <div className="accordion-item__content">{item.text}</div>
+    </div>
+  );
+}
